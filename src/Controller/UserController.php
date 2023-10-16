3ccc7f1da7c129126155b30bc20c27de9ebf23e1
KyNaOo <kevan122.wu@gmail.com>
2023-10-16 14:23:58 +0200

    fix:sécuriser l'url

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 #[Route('/user')]
 class UserController extends AbstractController
@@ -52,20 +53,28 @@ class UserController extends AbstractController
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+
     {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_home_page', [], Response::HTTP_SEE_OTHER);
+        $id= $request->attributes->get('id');
+        $userid=$this->getUser()->getId();
+        if($id==$userid){
+            $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->flush();
+    
+                return $this->redirectToRoute('app_home_page', [], Response::HTTP_SEE_OTHER);
+            }
+    
+            return $this->render('user/edit.html.twig', [
+                'user' => $user,
+                'form' => $form,
+            ]);
+        }else{
+            throw new AccessDeniedException('Access denied');
         }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+   
     }
 
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
