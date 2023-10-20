469770a0ce90ee0663dfe90f5295ff083079573e
KyNaOo <kevan122.wu@gmail.com>
2023-10-20 16:36:37 +0200

    feat:style crud admin all

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -6,18 +6,39 @@ use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class CompanyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('location')
-            ->add('creationDate')
-            ->add('revenues')
-            ->add('nbEmployees')
-            ->add('domain')
+            ->add('name', TextType::class,[
+                'label'=>false,
+                'required'=>true
+            ])
+            ->add('location', TextType::class,[
+                'label'=>false,
+                'required'=>true
+            ])
+            ->add('creationDate', DateTYpe::class,[
+                'label'=>false,
+                'years'=>range(date('Y')-100, date('Y'))
+            ])
+            ->add('revenues', TextType::class,[
+                'label'=>false,
+                'required'=>true
+            ])
+            ->add('nbEmployees',IntegerType::class,[
+                'label'=>false,
+                'required'=>true
+            ] )
+            ->add('domain', TextType::class,[
+                'label'=>false,
+                'required'=>true
+            ])
         ;
     }
 
