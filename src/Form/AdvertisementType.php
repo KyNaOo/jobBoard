df3a57619ff3a5998f3b4f6a51c851628a081165
KyNaOo <kevan122.wu@gmail.com>
2023-10-20 10:10:37 +0200

    feat:style postjob

--- src/Form/AdvertisementType.php
+++ src/Form/AdvertisementType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Advertisement;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,47 +17,26 @@ class AdvertisementType extends AbstractType
         $builder
             ->add('title', TextType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Titre"
-                ]
             ])
             ->add('description', TextareaType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Description"
-                ]
             ])
             ->add('location', TextType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Ville"
-                ]
             ])
             ->add('adress', TextType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Adresse"
-                ]
             ])
             ->add('wages', TextType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Salaire"
-                ]
             ])
             ->add('workingTime', TextType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Heures par semaine"
-                ]
             ])
             ->add('contract', TextType::class,[
                 'label'=> false,
-                'attr'=>[
-                    'placeholder'=>"Type de contrat"
-                ]
             ])
-            ->add('companyId')
+      
         ;
     }
 
